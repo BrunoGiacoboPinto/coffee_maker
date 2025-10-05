ac2954e475cd2a291d3f7f40ee3ec9df81ea5cb8
Bruno Giacobo Pinto <bruno.giacobo.pinto@gmail.com>
2025-10-05 15:10:26 -0300

    feat(firebase): enhance bootstrap process with environment-specific Firebase initialization
    - Introduced AppFlavor enum to manage different app environments (development, staging, production)
    - Updated bootstrap function to accept AppFlavor and initialize Firebase accordingly
    - Modified main files for each environment to utilize the new bootstrap function with appropriate flavor

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,11 +2,21 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:coffee_maker/di/injection.dart';
+import 'package:coffee_maker/firebase_options_development.dart' as dev;
+import 'package:coffee_maker/firebase_options_production.dart' as production;
+import 'package:coffee_maker/firebase_options_staging.dart' as staging;
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:logging/logging.dart';
 import 'package:stack_trace/stack_trace.dart';
+
+enum AppFlavor {
+  development,
+  staging,
+  production,
+}
 
 final class AppBlocObserver extends BlocObserver {
   const AppBlocObserver();
@@ -31,15 +41,29 @@ final class AppBlocObserver extends BlocObserver {
   }
 }
 
-Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
+Future<void> bootstrap(
+  FutureOr<Widget> Function() builder,
+  AppFlavor flavor,
+) async {
   return Chain.capture<void>(
     () async {
+      WidgetsFlutterBinding.ensureInitialized();
+
       Logger.root.level = kDebugMode ? Level.ALL : Level.INFO;
       Logger.root.onRecord.listen(
         (record) {
           if (kDebugMode) {
             print('${record.level.name}: ${record.time}: ${record.message}');
           }
+        },
+      );
+
+      await Firebase.initializeApp(
+        options: switch (flavor) {
+          AppFlavor.production =>
+            production.DefaultFirebaseOptions.currentPlatform,
+          AppFlavor.staging => staging.DefaultFirebaseOptions.currentPlatform,
+          AppFlavor.development => dev.DefaultFirebaseOptions.currentPlatform,
         },
       );
 
@@ -50,6 +74,7 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
           details.exception,
           trace.terse,
         );
+
         await FirebaseCrashlytics.instance.recordFlutterFatalError(details);
       };
 
