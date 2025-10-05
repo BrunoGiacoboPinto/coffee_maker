3bd0f2653afc82fbbfc83e5ff9736165cd4476b1
Bruno Giacobo Pinto <bruno.giacobo.pinto@gmail.com>
2025-10-04 23:46:04 -0300

    feat(di): configure coffee photos dependencies and services

--- lib/di/injection.dart
+++ lib/di/injection.dart
@@ -1,16 +1,53 @@
 import 'package:coffee_maker/di/injection.config.dart';
+import 'package:coffee_maker/home/bloc/home_bloc.dart';
+import 'package:coffee_photos_repository/coffee_photos_repository.dart';
+import 'package:dio/dio.dart';
+import 'package:environment/environment.dart';
 import 'package:get_it/get_it.dart';
-import 'package:injectable/injectable.dart';
+import 'package:hive_ce_flutter/hive_flutter.dart';
+import 'package:injectable/injectable.dart' hide Environment;
 import 'package:network/network.dart';
+import 'package:path_provider/path_provider.dart';
 
 final GetIt getIt = GetIt.instance;
+
+const _kPhotosBoxName = 'photos';
 
 @InjectableInit()
 Future<void> configureDependencies() async {
   getIt.init();
 
+  final dio = Dio(
+    BaseOptions(
+      baseUrl: Environment.apiBaseUrl,
+      connectTimeout: const Duration(seconds: Environment.connectTimeout),
+      receiveTimeout: const Duration(seconds: Environment.receiveTimeout),
+    ),
+  );
+
+  final directory = await getApplicationDocumentsDirectory();
+  Hive
+    ..init(directory.path)
+    ..registerAdapter(CoffeePhotoDataAdapter());
+
+  final photosBox = await Hive.openBox<CoffeePhotoData>(_kPhotosBoxName);
   final httpClientProvider = HttpClientProvider();
+
   getIt
+    ..registerLazySingleton<Dio>(() => dio)
+    ..registerLazySingleton<CoffeePhotosService>(() => CoffeePhotosService(dio))
+    ..registerLazySingleton<CoffeePhotosRepository>(
+      () => CoffeePhotosRepository(
+        coffeePhotosService: getIt<CoffeePhotosService>(),
+        photosBox: photosBox,
+      ),
+    )
+    ..registerFactory<HomeBloc>(
+      () => HomeBloc(
+        coffeePhotosRepository: getIt<CoffeePhotosRepository>(),
+      ),
+    )
+    ..registerLazySingleton<Box<CoffeePhotoData>>(() => photosBox)
     ..registerLazySingleton<HttpClientProvider>(() => httpClientProvider)
     ..registerLazySingleton<InternetProber>(
       () => InternetProber(httpClientProvider),
