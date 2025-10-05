33ee425abb7d5dd2a6f03a096f7f6f0ce000de1d
Bruno Giacobo Pinto <bruno.giacobo.pinto@gmail.com>
2025-10-05 01:14:34 -0300

    feat(favorites): implement favorites page UI with empty and loading states
    Complete redesign of favorites page with BLoC integration, shimmer loading state, empty state with helpful message, and list view for displaying favorite coffee photos using CoffeePhotoCard widget.

--- lib/favorites/view/favorites_page.dart
+++ lib/favorites/view/favorites_page.dart
@@ -1,36 +1,153 @@
+import 'package:coffee_maker/favorites/favorites.dart';
+import 'package:coffee_maker/widgets/coffee_photo_card.dart';
+import 'package:coffee_photos_repository/coffee_photos_repository.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:shimmer/shimmer.dart';
 
 final class FavoritesPage extends StatelessWidget {
-  const FavoritesPage({super.key});
+  const FavoritesPage({required this.favoritesBloc, super.key});
+
+  final FavoritesBloc favoritesBloc;
+
+  @override
+  Widget build(BuildContext context) =>
+      FavoritesView(favoritesBloc: favoritesBloc);
+}
+
+final class FavoritesView extends StatefulWidget {
+  @visibleForTesting
+  const FavoritesView({required this.favoritesBloc, super.key});
+
+  final FavoritesBloc favoritesBloc;
+
+  @override
+  State<FavoritesView> createState() => _FavoritesViewState();
+}
+
+class _FavoritesViewState extends State<FavoritesView> {
+  @override
+  void initState() {
+    super.initState();
+    widget.favoritesBloc.add(const FetchFavoritesEvent());
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const FavoritesView();
+    return BlocBuilder<FavoritesBloc, FavoritesState>(
+      bloc: widget.favoritesBloc,
+      builder: (context, state) {
+        return AnimatedSwitcher(
+          duration: const Duration(milliseconds: 300),
+          child: switch (state) {
+            FavoritesLoadingState() ||
+            FavoritesInitialState() => const _FavoritesViewLoading(),
+            FavoritesSuccessState() =>
+              state.photos.isEmpty
+                  ? const _FavoritesViewEmpty()
+                  : _FavoritesViewSuccess(
+                      photos: state.photos,
+                      onToggleFavorite: (id) {
+                        widget.favoritesBloc.add(ToggleFavoriteEvent(id));
+                      },
+                      onTap: (id) => context.push('/details/$id'),
+                    ),
+            FavoritesErrorState() => const SizedBox.expand(),
+            _ => const _FavoritesViewLoading(),
+          },
+        );
+      },
+    );
   }
 }
 
-final class FavoritesView extends StatelessWidget {
-  @visibleForTesting
-  const FavoritesView({super.key});
+final class _FavoritesViewLoading extends StatelessWidget {
+  const _FavoritesViewLoading();
 
   @override
   Widget build(BuildContext context) {
-    return GridView.custom(
-      gridDelegate: SliverQuiltedGridDelegate(
-        crossAxisCount: 4,
-        mainAxisSpacing: 4,
-        crossAxisSpacing: 4,
-        repeatPattern: QuiltedGridRepeatPattern.inverted,
-        pattern: const [
-          QuiltedGridTile(2, 2),
-          QuiltedGridTile(1, 1),
-          QuiltedGridTile(1, 1),
-          QuiltedGridTile(1, 2),
+    return Shimmer.fromColors(
+      baseColor: Colors.grey[300]!,
+      highlightColor: Colors.grey[100]!,
+      child: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) => Container(
+          height: 200,
+          margin: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            color: Colors.grey[300],
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+final class _FavoritesViewEmpty extends StatelessWidget {
+  const _FavoritesViewEmpty();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.favorite_border,
+            size: 64,
+            color: Colors.grey,
+          ),
+          SizedBox(height: 16),
+          Text(
+            'No favorites yet',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey,
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            'Tap the heart icon on photos to add them to your favorites',
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.grey,
+            ),
+            textAlign: TextAlign.center,
+          ),
         ],
       ),
-      childrenDelegate: SliverChildBuilderDelegate(
-        (context, index) => Container(color: Colors.blue),
+    );
+  }
+}
+
+final class _FavoritesViewSuccess extends StatelessWidget {
+  const _FavoritesViewSuccess({
+    required this.photos,
+    required this.onToggleFavorite,
+    required this.onTap,
+  });
+
+  final List<CoffeePhotoData> photos;
+  final ValueChanged<String> onToggleFavorite;
+  final ValueChanged<String> onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: photos.length,
+      itemBuilder: (context, index) => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: SizedBox(
+          height: 200,
+          child: CoffeePhotoCard(
+            photo: photos[index],
+            onToggleFavorite: onToggleFavorite,
+            onTap: onTap,
+          ),
+        ),
       ),
     );
   }
