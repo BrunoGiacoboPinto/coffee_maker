b41466fd89b944ecd5771b05527aa3ed4b2e3a3e
Bruno Giacobo Pinto <bruno.giacobo.pinto@gmail.com>
2025-10-04 23:58:11 -0300

    test: improve coffee photos repository test coverage

--- packages/coffee_photos_repository/test/src/coffee_photos_repository_test.dart
+++ packages/coffee_photos_repository/test/src/coffee_photos_repository_test.dart
@@ -1,13 +1,33 @@
-// Not required for test files
-// ignore_for_file: prefer_const_constructors
-
 import 'package:coffee_photos_repository/coffee_photos_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hive_ce/hive.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockCoffeePhotosService extends Mock implements CoffeePhotosService {}
+
+class MockPhotosBox extends Mock implements Box<CoffeePhotoData> {}
 
 void main() {
   group('CoffeePhotosRepository', () {
+    late CoffeePhotosRepository repository;
+    late MockCoffeePhotosService mockService;
+    late MockPhotosBox mockBox;
+
+    setUp(() {
+      mockService = MockCoffeePhotosService();
+      mockBox = MockPhotosBox();
+
+      // Setup mock box to return empty values list
+      when(() => mockBox.values).thenReturn([]);
+
+      repository = CoffeePhotosRepository(
+        coffeePhotosService: mockService,
+        photosBox: mockBox,
+      );
+    });
+
     test('can be instantiated', () {
-      expect(CoffeePhotosRepository(), isNotNull);
+      expect(repository, isNotNull);
     });
   });
 }
